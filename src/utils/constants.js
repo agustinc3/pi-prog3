f75060b43fd2d1afc9f3266499e3917dc8e3fc93
Ema Gondell <egondell@udesa.edu.ar>
2023-09-05 21:29:34 -0300

    utils

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export  const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZTYxNzNkNjJlNzU4MzU2YjU3NmY2YTI2YjAyODUzMiIsInN1YiI6IjYzNjUxOTZiMGQyZjUzMDA3ZGY2ZTEwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kRdXtcIN1cl6bNE4mhC_oA-zi3NRVX3kJtqycbAVcbs'
+  }
+};
